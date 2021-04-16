6d74558fd02b32f489e4116cd1ca69d9cf6926a4
Thariq Audah <thariq.audah@gmail.com>
2021-04-16 15:55:04 +0700

    Add search component

---
+++ src/components/SearchPlaylist.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="search-playlist">
+    <form @submit.prevent="handleSubmit">
+      <input type="text" placeholder="Search playlists..." v-model="keyword">
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'SearchPlaylist',
+  setup(props, context) {
+    const keyword = ref('');
+
+    const handleSubmit = () => {
+      context.emit('onSearch', keyword.value);
+    }
+
+
+    return { keyword, handleSubmit };
+  }
+}
+</script>
+
+<style>
+.search-playlist input {
+  padding: 16px 20px;
+  background: transparent;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home container">
     <div class="error" v-if="error">{{ error }}</div>
+    <SearchPlaylist @onSearch="filterPlaylists" />
     <ListView :playlists="playlists" />
   </div>
 </template>
@@ -8,15 +9,18 @@
 <script>
 import getCollection from '@/composables/getCollection';
 import ListView from '@/components/ListView.vue';
+import SearchPlaylist from '@/components/SearchPlaylist.vue';
 
 export default {
   name: 'Home',
-  components: { ListView },
+  components: { SearchPlaylist, ListView },
   setup() {
     // Get playlist with realtime data listener
     const { error, documents: playlists } = getCollection('playlists');
 
-    return { error, playlists };
+    const filterPlaylists = (value) => playlists.filter(playlist => playlist.title.includes(value));
+
+    return { error, playlists, filterPlaylists };
   },
 };
 </script>
